197aaa61be5595cdcfc894140b93aeec652cf1bc
alex <>
2019-10-22 21:03:03 +0300

    fix

--- bangcle_tool/src/com/fengyue/bangcle/AESUtil.java
+++ bangcle_tool/src/com/fengyue/bangcle/AESUtil.java
@@ -1,13 +1,10 @@
 package com.fengyue.bangcle;
 
-import java.util.Arrays;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class AESUtil {
 
@@ -17,54 +14,48 @@ public class AESUtil {
 	public static final String charset = "UTF-8";
 	//私钥
 	private static final String ASE_KEY="1234567812345678";   //AES固定格式为128/192/256 bits.即：16/24/32bytes。DES固定格式为128bits，即8bytes。
-	
-	private static String transformation = "AES/CBC/PKCS5Padding"; 
-	
+
+	private static String transformation = "AES/CBC/PKCS5Padding";
+
 	private static String algorithm = "AES";
 
-	
-	public static byte[] encrypt(String content, String key) {  
-        try {  
-            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), algorithm);  
-            IvParameterSpec iv = new IvParameterSpec(key.getBytes());  
-            Cipher cipher = Cipher.getInstance(transformation);  
-            byte[] byteContent = content.getBytes(charset);  
-            
-            cipher.init(Cipher.ENCRYPT_MODE, skey, iv);// 初始化  
-            byte[] result = cipher.doFinal(byteContent);  
-            return result; // 加密  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-        return null;  
-    }  
-	
-	public static byte[] encrypt(byte[] content, String key) {  
-        try {  
-            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), algorithm);  
-            IvParameterSpec iv = new IvParameterSpec(key.getBytes());  
-            Cipher cipher = Cipher.getInstance(transformation);  
-            
-            cipher.init(Cipher.ENCRYPT_MODE, skey, iv);// 初始化  
-            byte[] result = cipher.doFinal(content);  
-            return result; // 加密  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-        return null;  
-    }  
-	
-	
 
-	/**
-	 * 解密
-	 * 
-	 * @param encrypted
-	 * @return
-	 */
+	public static byte[] encrypt(String content, String key) {
+        try {
+            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), algorithm);
+            IvParameterSpec iv = new IvParameterSpec(key.getBytes());
+            Cipher cipher = Cipher.getInstance(transformation);
+            byte[] byteContent = content.getBytes(charset);
+
+            cipher.init(Cipher.ENCRYPT_MODE, skey, iv);// 初始化
+            byte[] result = cipher.doFinal(byteContent);
+            return result; // 加密
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+	public static byte[] encrypt(byte[] content, String key) {
+        try {
+            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), algorithm);
+            IvParameterSpec iv = new IvParameterSpec(key.getBytes());
+            Cipher cipher = Cipher.getInstance(transformation);
+
+            cipher.init(Cipher.ENCRYPT_MODE, skey, iv);// 初始化
+            byte[] result = cipher.doFinal(content);
+            return result; // 加密
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
 	public static String decrypt(String encryptData) {
 		try {
-            byte[] tmp=new BASE64Decoder().decodeBuffer(encryptData);
+            byte[] tmp=Base64.getDecoder().decode(encryptData);
             byte[]  iv=new byte[16];
             Arrays.fill(iv, (byte)0);
 			//IvParameterSpec zeroIv = new IvParameterSpec(iv);
@@ -77,13 +68,13 @@ public class AESUtil {
 			byte[] decryptedData = cipher.doFinal(tmp);
 			System.out.println("解密后 hex data:"+byte2hex(decryptedData));
 			System.out.println("加密后 hex data len:"+decryptedData.length);
-			return new String(decryptedData, charset); 
+			return new String(decryptedData, charset);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
 		}
 	}
-	
+
 	/**
      * 字节数组转成16进制字符串
      * @param b
@@ -102,7 +93,7 @@ public class AESUtil {
         }
         return sb.toString().toUpperCase(); // 转成大写
     }
-    
+
     /**
      * 将hex字符串转换成字节数组
      * @param inputString
@@ -122,5 +113,5 @@ public class AESUtil {
         return result;
     }
 
-	
+
 }
